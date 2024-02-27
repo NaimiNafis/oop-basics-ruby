d67fc1f569f6d3c7aa88f57a46a969b9a0389dd4
MeeSup <126528516+MeeSup@users.noreply.github.com>
2024-02-27 22:44:13 +0800

    Add check_win conditions for row col diagonal

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,6 +1,10 @@
 require 'pry-byebug'
 
-grid = Array.new(3) { Array.new(3, " ") }
+# grid = Array.new(3) { Array.new(3, " ") }
+
+grid = [[" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "],]
 
 def board_display(grid)
   grid.each do |row|
@@ -13,14 +17,28 @@ board_display(grid)
 def check_win(grid)
   # state always false unless win condition met
 
-  grid.each_with_index do |row, i|
-    if grid.uniq.length == 1 && grid.first != " "
+  # check each row and column
+  3.times do |i|
+    if grid[i].uniq.length == 1 && grid[i].first != " "
+      return true
+    elsif grid.transpose[i].uniq.length == 1 && grid.transpose[i].first != " "
       return true
     end
   end
 
+  # check diagonal
+  diagonal1 = [grid[0][0], grid[1][1], grid[2][2]]
+  diagonal2 = [grid[0][2], grid[1][1], grid[2][0]]
+  if diagonal1.uniq.length == 1  && diagonal1.first != " "
+    return true
+  elsif diagonal2.uniq.length == 1  && diagonal2.first != " "
+    return true
+  end
+
   false
 end
+
+check_win(grid)
 
 def game
 end
