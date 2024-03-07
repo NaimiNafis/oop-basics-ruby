97739805f1e9a76d8f4b7b792306102a05c973f1
MeeSup <126528516+MeeSup@users.noreply.github.com>
2024-03-07 10:23:58 +0800

    Refactor role_choice and start_game using rubocop

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Game class manage game flow, including choosing roles, starting the game, and managing game attempts.
 class Game
-  COLOURS = ["red", "green", "blue", "yellow", "purple", "orange"]
+  COLOURS = %w[red green blue yellow purple orange].freeze
   MAX_ATTEMPTS = 10
   CODE_LENGTH = 4
 
@@ -8,74 +11,107 @@ class Game
     @creator = nil
     @guesser = nil
     @secret_colours = []
+    @correct_positions = 0  # Add this line
   end
 
+
   def choose_roles
-    puts "Do you want to be the (1) Code Creator or (2) Code Guesser?"
-    choice = gets.chomp
-    loop do
-      if choice == '1'
-        @creator = Player.new
-        @guesser = Computer.new
-        @secret_colours = @creator.create_code(COLOURS, CODE_LENGTH)
-        break
-      elsif choice == '2'
-        @creator = Computer.new
-        @guesser = Player.new
-        @secret_colours = @creator.create_code(COLOURS, CODE_LENGTH)
-        break
-      else
-        puts "Wrong input: Please enter 1 or 2 only."
-        choice = gets.chomp
-      end
-    end
+    setup_roles(role_choice)
     start_game
   end
 
   private
 
-  def start_game
-    clear_screen
-    while @attempts < MAX_ATTEMPTS do
-      puts "Previous Attempts:" unless @attempts == 0
-      # Optionally print a summary of previous attempts here
+  def role_choice
+    puts 'Do you want to be the (1) Code Creator or (2) Code Guesser?'
+    choice = gets.chomp
+    loop do
+      return choice if %w[1 2].include?(choice)
 
-      guess = @guesser.guess_code(COLOURS, CODE_LENGTH)
-      correct_positions = @secret_colours.zip(guess).count { |a,b| a == b }
-      total_correct_colours = @secret_colours.uniq.sum { |c| [@secret_colours.count(c), guess.count(c)].min }
-      correct_colours = total_correct_colours - correct_positions
-
-      clear_screen
-      puts "Attempt #{@attempts + 1}"
-      puts "Your guess: #{guess.join(', ')}"
-      puts "● Bagels (correct position): #{correct_positions}"
-      puts "○ Picos (correct color, wrong position): #{correct_colours}\n\n"
-
-      if correct_positions == CODE_LENGTH
-        puts "Congratulations, you've guessed the secret colors!"
-        break
-      end
-
-      @attempts += 1
-      if @attempts == MAX_ATTEMPTS
-        puts "You've reached the maximum attempts! The secret colors were: #{@secret_colours.join(', ')}."
-        break
-      else
-        puts "Try again! (Press enter to continue...)"
-        gets
-        clear_screen
-      end
+      puts 'Wrong input: Please enter 1 or 2 only.'
+      choice = gets.chomp
     end
   end
 
-  def clear_screen
-    system("clear") || system("cls")
+  def setup_roles(choice)
+    if choice == '1'
+      @creator = Player.new
+      @guesser = Computer.new
+    else
+      @creator = Computer.new
+      @guesser = Player.new
+    end
+    @secret_colours = @creator.create_code(COLOURS, CODE_LENGTH)
   end
 
+  def start_game
+    clear_screen
+    while @attempts < MAX_ATTEMPTS
+      display_previous_attempts
+      process_guess
+
+      break if game_over?
+
+      prepare_next_attempt
+    end
+  end
+
+  def display_previous_attempts
+    puts 'Previous Attempts:' unless @attempts.zero?
+    # Optionally print a summary of previous attempts here
+  end
+
+  def process_guess
+    guess = @guesser.guess_code(COLOURS, CODE_LENGTH)
+    @correct_positions, correct_colours = evaluate_guess(guess)
+
+    clear_screen
+    display_guess_feedback(guess, @correct_positions, correct_colours)
+
+    @attempts += 1
+  end
+
+
+  def evaluate_guess(guess)
+    correct_positions = @secret_colours.zip(guess).count { |a, b| a == b }
+    total_correct_colours = @secret_colours.uniq.sum { |c| [@secret_colours.count(c), guess.count(c)].min }
+    correct_colours = total_correct_colours - correct_positions
+    [correct_positions, correct_colours]
+  end
+
+  def display_guess_feedback(guess, correct_positions, correct_colours)
+    puts "Attempt #{@attempts + 1}"
+    puts "Your guess: #{guess.join(', ')}"
+    puts "● Bagels (correct position): #{correct_positions}"
+    puts "○ Picos (correct color, wrong position): #{correct_colours}\n\n"
+  end
+
+  def game_over?
+    if @correct_positions == CODE_LENGTH
+      puts "Congratulations, you've guessed the secret colors!"
+      true
+    elsif @attempts == MAX_ATTEMPTS
+      puts "You've reached the maximum attempts! The secret colors were: #{@secret_colours.join(', ')}."
+      true
+    else
+      false
+    end
+  end
+
+
+  def prepare_next_attempt
+    puts 'Try again! (Press enter to continue...)'
+    gets
+    clear_screen
+  end
+
+  def clear_screen
+    system('clear') || system('cls')
+  end
 end
 
+# Player class manages player actions including creating and guessing the code based on user input.
 class Player
-
   def create_code(colours, length)
     puts "Enter your secret code (#{colours.join(', ')}):"
     code = gets.chomp.split
@@ -97,8 +133,8 @@ class Player
   end
 end
 
+# Computer class handles computer actions such as generating a secret code and making guesses.
 class Computer
-
   def create_code(colours, length)
     colours.sample(length)
   end
